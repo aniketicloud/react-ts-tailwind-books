fa46cc0b6183b4f8b3ab1a66cc1b48f70af808c1
Aniket Pandharabale <aniketicloud@gmail.com>
2023-08-20 17:34:14 +0530

    second attempt: styling button and input of form

--- src/components/BookCreate.tsx
+++ src/components/BookCreate.tsx
@@ -20,30 +20,27 @@ export const BookCreate: React.FC<BookCreateProps> = ({ onCreate }) => {
   };
 
   return (
-    <div className="fixed bottom-0 w-screen bg-green-300 px-10 pb-8 pt-5">
+    <div className="fixed bottom-0 w-screen bg-green-400 px-10 pb-8 pt-5">
       <h3 className="text-center text-2xl font-bold text-white">Add a book</h3>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-6 grid gap-6 md:grid-cols-2">
-          <div className="relative h-10 w-full min-w-[200px]">
-            <input
-              className="border-blue-gray-200 text-blue-gray-700 placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 disabled:bg-blue-gray-50 peer h-full w-full rounded-[7px] border border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal outline outline-0 transition-all placeholder-shown:border focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0"
-              type="text"
-              id="title"
-              name="title"
-              placeholder=" "
-              value={title}
-              onChange={handleChange}
-            />
-            <label
-              className="before:content[' '] after:content[' '] text-blue-gray-400 before:border-blue-gray-200 after:border-blue-gray-200 peer-placeholder-shown:text-blue-gray-500 peer-disabled:peer-placeholder-shown:text-blue-gray-500 pointer-events-none absolute -top-1.5 left-0 flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all before:pointer-events-none before:mr-1 before:mt-[6.5px] before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-l before:border-t before:transition-all after:pointer-events-none after:ml-1 after:mt-[6.5px] after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-r after:border-t after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-l-2 peer-focus:before:border-t-2 peer-focus:before:border-pink-500 peer-focus:after:border-r-2 peer-focus:after:border-t-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent"
-              htmlFor="title"
-            >
-              Title
-            </label>
-          </div>
+      <form className="grid grid-flow-col gap-2" onSubmit={handleSubmit}>
+        <div className="grid grid-flow-col items-center justify-center">
+          <label className="hidden" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="w-28 rounded-full bg-green-100 px-4 py-2 text-sm transition-all duration-200 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-green-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Enter a book"
+            value={title}
+            onChange={handleChange}
+          />
         </div>
+
         <button
-          className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300  sm:w-auto"
+          disabled={!title}
+          className="inline-block rounded-full bg-green-200 px-4 py-2 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-green-300 focus:bg-green-300 focus:outline-none focus:ring focus:ring-green-300 focus:ring-offset-2 active:bg-green-500 disabled:cursor-not-allowed disabled:bg-stone-400"
           type="submit"
         >
           Create
